ddb0ce615a72a9b4204ca153eb9dcec21d98bd66
Ethan Lin <eragon9356@gmail.com>
2024-06-06 00:25:15 -0700

    Drivable car added/Clean up files
    Deleted "clutter"
    Added temp car sprite.
    Car is drivable with WASD.

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -19,20 +19,12 @@ class Load extends Phaser.Scene {
         //loading assets
         this.load.path = './assets/';
         // load graphics assets
-        this.load.image('platform', 'img/paddle.png');
-        this.load.image('particle', 'img/particle.png');
-        this.load.image('ground', 'img/ground.png');
-        this.load.image('foreground', 'img/foreground_buildings.png');
-        this.load.image('midground', 'img/midground_buildings.png');
-        this.load.image('background', 'img/background_buildings.png');
-        this.load.image('nightSky', 'img/night_sky.png');
+        // this.load.image('name', 'img/path.png');
+        this.load.image('car', 'img/car.png');
+        
         // load audio assets
-        this.load.audio('beats', ['audio/beats.mp3']);
-        this.load.audio('intro', ['audio/intro.mp3']);
-        this.load.audio('clang', ['audio/clang.mp3']);
-        this.load.audio('jump_sound', ['audio/slime_jump.mp3']);
-        this.load.audio('platform', ['audio/platform.mp3']);
-        this.load.audio('death', ['audio/death.mp3']);
+        // this.load.audio('name', ['audio/music.mp3']);
+        
         // load font
         this.load.bitmapFont('gem', 'font/gem.png', 'font/gem.xml');
 
