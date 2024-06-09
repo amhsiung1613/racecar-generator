9566894bf59f3e96bd2984ec4e489e8ce39499af
Ethan Lin <eragon9356@gmail.com>
2024-06-09 14:45:13 -0700

    Boundaries set
    Todo:
    Obstacle spawns
    New sprites/background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,18 +8,8 @@ class Play extends Phaser.Scene {
         this.deceleration = 5;
         this.rotationSpeed = 0.05;
 
-        this.my.trackArray = [];
-        this.trackPath = [];
-        this.directions = [
-            { x: 1, y: 0, angle: 0 },    // Right
-            { x: -1, y: 0, angle: 180 }, // Left
-            { x: 0, y: 1, angle: 90 },   // Down
-            { x: 0, y: -1, angle: -90 }  // Up
-        ];
-        this.placedPositions = new Set();
-        this.startX = 0;
-        this.startY = 0;
-        this.maxAttempts = 10;
+        this.spawnDelay = 2000; // Initial spawn delay for traffic
+        // this.my.sprite.enemy = [];
     }
 
     create() {
@@ -30,7 +20,7 @@ class Play extends Phaser.Scene {
         my.sprite.car.setCollideWorldBounds(true);
         my.sprite.car.setDepth(1);
 
-        this.physics.world.setBounds(0, 0, game.config.width, game.config.height);
+        this.physics.world.setBounds(0, game.config.height * 0.25, game.config.width, game.config.height);
         this.cameras.main.setBounds(0, 0, game.config.width, game.config.height);
         this.cameras.main.startFollow(my.sprite.car);
 
@@ -45,6 +35,17 @@ class Play extends Phaser.Scene {
 
         // Display background image
         this.backgroundImage = this.add.tileSprite(0, 0, 700, 700, "road").setOrigin(0, 0);
+
+        // Group for traffic
+        this.trafficGroup = this.add.group();
+
+        // Initial traffic spawn event
+        this.spawnTrafficEvent = this.time.addEvent({
+            delay: this.spawnDelay,
+            callback: this.spawnTraffic,
+            callbackScope: this,
+            loop: true
+        });
     }
 
     update() {
@@ -60,13 +61,6 @@ class Play extends Phaser.Scene {
         );
 
         let isOnTrack = true;
-
-        for (let track of this.my.trackArray) {
-            if (this.collides(track, car)) {
-                isOnTrack = true;
-                break;
-            }
-        }
 
         if (isOnTrack) {
             if (this.left.isDown) {
@@ -92,11 +86,47 @@ class Play extends Phaser.Scene {
             this.carSpeed = 10;
             console.log("car off track");
         }
+
+        //////
+
+        // Move and wrap background image
+        // this.backgroundImage.tilePositionY -= this.speed;
+        // this.wrapBackground();
     }
 
     collides(a, b) {
         if (Math.abs(a.x - b.x) > (a.displayWidth / 2 + b.displayWidth / 2)) return false;
         if (Math.abs(a.y - b.y) > (a.displayHeight / 2 + b.displayHeight / 2)) return false;
         return true;
+    }
+
+    setTrafficVelocity(speed) {
+        this.trafficGroup.getChildren().forEach((traffic) => {
+            traffic.setVelocityY(speed * 0.5); // Slow down the traffic speed
+        });
+    }
+
+    wrapBackground() {
+        const bgHeight = this.backgroundImage.height;
+
+        if (this.backgroundImage.y <= -bgHeight) {
+            this.backgroundImage.y = this.sys.game.config.height;
+        }
+
+        if (this.backgroundImage.y >= this.sys.game.config.height) {
+            this.backgroundImage.y = -bgHeight;
+        }
+    }
+
+    spawnTraffic() {
+        console.log("test");
+        const trafficSprites = ["corner", "long", "med"];
+        const spawnPositions = [200, 300, 400, 500];
+        const randomSprite = Phaser.Utils.Array.GetRandom(trafficSprites);
+        const randomX = Phaser.Utils.Array.GetRandom(spawnPositions);
+        const traffic = this.physics.add.sprite(randomX, -150, randomSprite).setScale(0.3);
+        // Set a smaller rectangle for the traffic hitbox
+        traffic.setVelocityY(this.speed); // Initial velocity to match the road speed
+        this.trafficGroup.add(traffic);
     }
 }
